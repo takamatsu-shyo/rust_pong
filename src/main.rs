9db1f192e72027b02d9b55e97ce1a56954a43614
Sho Yano <takamatsu.shyo@gmail.com>
2022-04-09 23:18:37 +0300

    mod

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
-use std::net::UdpSocket;
+mod hw;
 
-fn main() -> std::io::Result<()> {
-    {
-        let socket = UdpSocket::bind("127.0.0.1:50001")?;
-    
-        let mut buf = [0; 10];
-        let (amt, src) = socket.recv_from(&mut buf)?;
-    
-        let buf = &mut buf[..amt];
-        buf.reverse();
-        socket.send_to(buf, &src)?;
-    }
-    Ok(())
+fn main(){
+    hw::hello_world();
+
+    //{
+    //    let socket = UdpSocket::bind("127.0.0.1:50001")?;
+    //
+    //    let mut buf = [0; 10];
+    //    let (amt, src) = socket.recv_from(&mut buf)?;
+    //
+    //    let buf = &mut buf[..amt];
+    //    buf.reverse();
+    //    socket.send_to(buf, &src)?;
+    //}
+    //Ok(())
 }
